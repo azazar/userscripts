5149e0be39eb08a65c46b98a64c5341ca8fdf6c9
Mikhail Yevchenko <m..@uo1.net>
2024-06-03 08:03:04 +0300

    Use smaller button

--- add-fantlab-links-to-author-today.user.js
+++ add-fantlab-links-to-author-today.user.js
@@ -7,6 +7,7 @@
 // @description Добавляем на author.today ссылки на поиск fantlab
 // @updateURL   https://raw.githubusercontent.com/azazar/userscripts/main/add-fantlab-links-to-author-today.user.js
 // @downloadURL https://raw.githubusercontent.com/azazar/userscripts/main/add-fantlab-links-to-author-today.user.js
+// @grant       GM_addStyle
 // ==/UserScript==
 
 (function() {
@@ -19,23 +20,16 @@
      * @return {boolean} - Returns true if all children are text nodes, otherwise false.
      */
     function isTextOnly(domElement) {
-      // Get all child nodes of the element
-      const childNodes = domElement.childNodes;
-
-      if (childNodes.length === 0) {
-        return false;
-      }
-
-      // Iterate through the child nodes
-      for (let i = 0; i < childNodes.length; i++) {
-        // If the node is not a text node, return false
-        if (childNodes[i].nodeType !== Node.TEXT_NODE) {
-          return false;
+        const childNodes = domElement.childNodes;
+        if (childNodes.length === 0) {
+            return false;
         }
-      }
-
-      // If all child nodes are text nodes, return true
-      return true;
+        for (let i = 0; i < childNodes.length; i++) {
+            if (childNodes[i].nodeType !== Node.TEXT_NODE) {
+                return false;
+            }
+        }
+        return true;
     }
 
     function addFantlabLinks(root) {
@@ -43,41 +37,34 @@
             if (feedRowElement.fantlabLinksAdded) {
                 return;
             }
-
             feedRowElement.fantlabLinksAdded = true;
 
             let authors = [];
-
             let anchors = Array.from(feedRowElement.querySelectorAll('a[href]')).filter(a => isTextOnly(a));
 
             anchors.forEach(anchorElement => {
                 let href = anchorElement.getAttribute('href');
-
                 let userMatch = /^\/u\/([^\/]+)/.exec(href);
 
                 if (userMatch) {
                     authors.push(anchorElement.textContent.trim());
-
-                    anchorElement.insertAdjacentHTML('afterend', ` (<a href="https://fantlab.ru/searchmain?searchstr=${encodeURIComponent(anchorElement.textContent.trim())}">FantLab</a>)`);
+                    anchorElement.insertAdjacentHTML('afterend', `<a href="https://fantlab.ru/searchmain?searchstr=${encodeURIComponent(anchorElement.textContent.trim())}"><img class="userscript-fl" src="https://fantlab.ru/favicon.ico"></a>`);
                 }
             });
 
             anchors.forEach(anchorElement => {
                 let href = anchorElement.getAttribute('href');
-
                 let workMatch = /^\/work\/(\d+)/.exec(href);
 
                 if (workMatch) {
-                    anchorElement.insertAdjacentHTML('afterend', ` (<a href="https://fantlab.ru/searchmain?searchstr=${encodeURIComponent(anchorElement.textContent.trim() + ' ' + authors.join(' '))}">FantLab</a>)`);
+                    anchorElement.insertAdjacentHTML('afterend', `<a href="https://fantlab.ru/searchmain?searchstr=${encodeURIComponent(anchorElement.textContent.trim() + ' ' + authors.join(' '))}"><img class="userscript-fl" src="https://fantlab.ru/favicon.ico"></a>`);
                 }
             });
         });
     }
 
-    // Initial run on page load
     addFantlabLinks(document.body);
 
-    // Observe changes to the body and dynamically apply the function to new elements
     const observer = new MutationObserver(mutations => {
         mutations.forEach(mutation => {
             mutation.addedNodes.forEach(node => {
@@ -93,3 +80,5 @@
         subtree: true
     });
 })();
+
+GM_addStyle('.userscript-fl { margin-left: 5px; vertical-align: middle; height: 1em }');
