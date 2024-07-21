831e192f4f18dcc9bdb3e2939c09e0a219b71fe7
Mikhail Yevchenko <m..@uo1.net>
2024-07-21 21:00:10 +0300

    Fix description

--- paywall-bypass.user.js
+++ paywall-bypass.user.js
@@ -4,7 +4,7 @@
 // @match       *://*/*
 // @run-at      document-start
 // @version     0.1
-// @description Attempts to bypass paywalls on various websites
+// @description Attempts to bypass economistcom paywall. And other paywalls that exist on various websites in the future.
 // @updateURL   https://raw.githubusercontent.com/azazar/userscripts/main/paywall-bypass.user.js
 // @downloadURL https://raw.githubusercontent.com/azazar/userscripts/main/paywall-bypass.user.js
 // @license     MIT
